9cc15f88aaa7551273079bc683bae45ed44556ab
Tarik Fayad <tarikfayad@protonmail.com>
2025-01-23 17:34:54 -0500

    Added a basic time function and hooked it up to the Live_view.

--- Pento/pento/lib/pento_web/live/wrong_live.ex
+++ Pento/pento/lib/pento_web/live/wrong_live.ex
@@ -2,7 +2,7 @@ defmodule PentoWeb.WrongLive do
   use PentoWeb, :live_view
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, score: 0, message: "Make a guess:")}
+    {:ok, assign(socket, score: 0, message: "Make a guess:", time: time())}
   end
 
   def render(assigns) do
@@ -10,6 +10,7 @@ defmodule PentoWeb.WrongLive do
     <h1 class="mb-4 text-4xl font-extrabold">Your score: <%= @score %></h1>
     <h2>
       <%= @message %>
+      It's <%= @time %>
     </h2>
     <br/>
     <h2>
@@ -24,6 +25,12 @@ defmodule PentoWeb.WrongLive do
     """
   end
 
+  def time() do
+    # Time doesn't update on the LiveView because the socket doesn't have a time value associated to it.
+    # However, by adding time to the socket, and but assigning it when clicking a button in handle_event we see it updated.
+    DateTime.utc_now |> to_string
+  end
+
   # Letting pattern matching do the heavy lifting. handle_event will only trigger for functions where the first argument is "guess".
   # The second property is accessing the map with a key of "number" from "phx-value-number" and assigning it to the variable guess.
   # The socket is passed in so that we can update the live view which we do via socket.assigns. We assigned score to it on line 5 as well as message.
@@ -35,7 +42,8 @@ defmodule PentoWeb.WrongLive do
       assign(
         socket,
         message: message,
-        score: score
+        score: score,
+        time: time()
       )
     }
   end
